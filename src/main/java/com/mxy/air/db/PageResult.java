fdd5935207c47ef7d73d97894718c7058582e4e4
mengxiangyun <718517636@qq.com>
2018-06-04 16:17:40 +0800

    枚举改成类, 另定义ATTRIBUTE属性

--- src/main/java/com/mxy/air/db/PageResult.java
+++ src/main/java/com/mxy/air/db/PageResult.java
@@ -1,13 +1,34 @@
 package com.mxy.air.db;
 
+import java.util.List;
+import java.util.Map;
+
+import com.mxy.air.json.JSONObject;
+
 /**
  * 分页结果
  * 
  * @author mengxiangyun
  *
  */
-public enum PageResult {
+public class PageResult {
 
-	START, END, TOTAL, DATAS
+	/**
+	 * 分页结果属性
+	 * 
+	 * @author mengxiangyun
+	 *
+	 */
+	enum ATTRIBUTE {
+		START, END, TOTAL, DATA
+	}
+
+	public PageResult() {
+	}
+	
+	public static JSONObject wrap(long start, long end, long total, List<Map<String, Object>> list) {
+		return new JSONObject().put(ATTRIBUTE.START, start).put(ATTRIBUTE.END, end).put(ATTRIBUTE.TOTAL, total)
+				.put(ATTRIBUTE.DATA, list);
+	}
 
 }
